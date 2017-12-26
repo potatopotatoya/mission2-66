21b28ee5335d066f2010df3d6fc3747309ad37ed
potatopotatoya <34781678+potatopotatoya@users.noreply.github.com>
2017-12-26 09:20:12 +0900

    Add files via upload

---
+++ mission_2-12.php
@@ -0,0 +1,17 @@
+﻿<!--selectでテーブルの中身を表示-->
+<?php
+header('Content-Type: text/html; charset = UTF-8');
+$dsn = 'mysql:dbname=co_627_it_99sv_coco_com;host=localhost';
+$user = 'co-627.it.99sv-c';
+$password = 'ub7Vcy4';
+$pdo = new PDO($dsn,$user,$password);
+
+$sqp = 'SELECT*FROM tennis;';
+$results = $pdo -> query($sql);
+
+foreach($results as $row){
+	echo $row['id'].',';
+	echo $row['name'].',';
+	echo $row['comment'].'<br>';
+}
+?>
