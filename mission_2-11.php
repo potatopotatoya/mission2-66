dc852f787ea048a4e9a3828a1361cf4fc006b12a
potatopotatoya <34781678+potatopotatoya@users.noreply.github.com>
2017-12-26 09:25:38 +0900

    Add files via upload

---
+++ mission_2-11.php
@@ -0,0 +1,14 @@
+﻿<!--insertを使ってデータを入力-->
+<?php
+header('Content-Type: text/html; charset = UTF-8');
+$dsn = 'mysql:dbname=co_627_it_99sv_coco_com;host=localhost';
+$user = 'co-627.it.99sv-c';
+$password = 'ub7Vcy4';
+$pdo = new PDO($dsn,$user,$password);
+$sql = $pdo ->prepare("INSERT INTO tennis(id, name, comment) VALUES('1',:name,:comment);");
+$sql -> bindParam(':name',$name, PDO::PARAM_STR);
+$sql -> bindParam(':comment', $comment, PDO::PARAM_STR);
+$name = '名前';
+$comment = 'コメント';
+$sql -> execute();
+?>
